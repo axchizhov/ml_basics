b0ee22c66335c5bcf83bc00ac91d5bff73d0bb7f
axchizhov <66429249+axchizhov@users.noreply.github.com>
2024-06-19 02:01:28 +0300

    update formatting

--- linear_regression.py
+++ linear_regression.py
@@ -3,32 +3,33 @@ import numpy as np
 
 np.random.seed(42)
 
+
 class LinearRegression:
     def __init__(self):
         self.weights = None
         self.learning_rate = 0.001
-    
+
     def fit(self, X, y):
         n_samples, n_features = X.shape
         # self.weights = np.random.randn(n_features, 1)
         self.weights = np.zeros((n_features, 1))
 
         epochs = 10
-        
+
         for _ in range(epochs):
             y_pred = self.predict(X)
-            
-            loss = (y - y_pred)**2
+
+            loss = (y - y_pred) ** 2
             loss = sum(loss) / n_samples
             print(loss)
-            
+
             grad = -2 * X.T @ (y - y_pred)
             self.weights -= self.learning_rate * grad
-    
+
     def predict(self, X):
         y_pred = X @ self.weights
         return y_pred
-            
+
 
 # %%
 
